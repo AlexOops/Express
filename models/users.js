d36c0a1ef0788c4a810cc53a603ef8df21f96e9f
Alex <8768905213@mail.ru>
2022-12-16 11:03:50 +0300

    auth with jwt access|refresh

--- models/users.js
+++ models/users.js
@@ -1,23 +1,31 @@
 import mongoose, {model} from "mongoose";
+import bcrypt from "bcrypt";
 
 const {Schema} = mongoose;
 
 const UsersSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
-    phone: {
+    password: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
-        required: true
-    },
-
-    bufferCommands: false,
-    autoCreate: false // disable `autoCreate` since `bufferCommands` is false
+        required: true,
+        unique: true
+    }
 });
+//
+UsersSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+// UsersSchema.methods.isValidPassword = async function (password) {
+//     return await bcrypt.compare(password, this.password);
+// }
 
 export const Users = model('Users', UsersSchema, 'users');
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 import ChatRouter from './routes/chats.js';
 import MessageRouter from './routes/messages.js';
-import UsersRouter from './routes/users.js';
+import AuthRouter from './routes/auth.js';
 
 import {errorMiddleware} from "./middlewares/error.js";
+import {tokenVerify} from "./middlewares/tokenVerify.js";
 
 mongoose.set('strictQuery', true);
 mongoose.set('bufferCommands', false);
 
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDb started"))
     .catch((err) => console.log(err));
 
@@ -22,13 +25,20 @@ app.use(express.json())
 app.get('/', (_, res) => {
     res.send('OK');
 })
+app.get('/profile', tokenVerify, (req, res) => {
+    res.send("I'm secured");
+});
 
+app.use('/', AuthRouter);
 app.use('/chats', ChatRouter);
 app.use('/messages', MessageRouter);
-app.use('/users', UsersRouter);
 
 app.use(errorMiddleware);
 
-app.listen(5555, () => console.log("Server has been started to https://localhost:5555"));
+app.all("*", (_, res) => {
+    res.status(404).json({error: 404});
+});
+
+app.listen(process.env.PORT, () => console.log("Server has been started to http://localhost:5555"));
 
 
