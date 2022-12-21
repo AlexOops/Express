ad4262beefbb659feed06102e322ac4fe7c8b9c1
Alex <8768905213@mail.ru>
2022-12-21 15:53:36 +0300

    created simple chat on sockets io

--- websockets/index.js
+++ websockets/index.js
@@ -6,8 +6,11 @@ import http from 'http';
 const server = http.createServer(app);
 import {Server} from 'socket.io';
 import path from "path";
-
 import cors from 'cors';
+
+// random name
+import {generateSlug} from "random-word-slugs";
+
 app.use(cors());
 
 const io = new Server(server);
@@ -17,29 +20,49 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve('./index.html'));
 });
 
-const userMap = {}
+const userMap = {};
 
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-    userMap[socket.id] = {
-        id: socket.id
+io.on('connection', (client) => {
+    console.log('user connected', client.id);
+
+    userMap[client.id] = {
+        id: client.id,
+        name: generateSlug()
     }
 
-    console.log(userMap)
+    const info = {
+        connect: `Подключился пользователь ${userMap[client.id].name}`,
+        userCounter: io.engine.clientsCount
+    }
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        delete userMap[socket.id]
+    console.log(info.userCounter)
+
+    client.broadcast.emit('connection', info);
+    client.emit('connection', info);
+
+
+    client.on('disconnect', () => {
+        console.log('user disconnected', client.id);
+
+        const info = {
+            loggedOut: `Отключился пользователь ${userMap[client.id].name}`
+        }
+
+        client.broadcast.emit('room-out', info);
+        client.emit('room-out', info);
+
+        delete userMap[client.id]
     });
 
-    socket.on('client-msg', (data) => {
+    client.on('client-msg', (data) => {
         console.log(data);
 
         const payload = {
-            message: data.message.split('').join('')
+            message: data.message.split('').join(''),
+            author: userMap[client.id].name
         }
-        socket.broadcast.emit('client-msg', payload); // на всех
-        socket.emit('client-msg', payload); // на себя
+        client.broadcast.emit('client-msg', payload); // на всех
+        client.emit('client-msg', payload); // на себя
 
     })
 })
